854305bba96b8e22769c4f33a814ae782e5fc678
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2021-09-09 00:36:20 +0700

    Adjust to move backward

--- main.js
+++ main.js
@@ -23,59 +23,99 @@ class Field {
 
     switch (move.toLowerCase()) {
       case 'r':
-        console.log('Move Right')
-        this.x += 1;
+        if (this.field[this.y][this.x + 1] !== pathCharacter){
+          console.log('Move Right Forward');
+          this.x += 1;
+        } else {
+          console.log('Move Right Backward')
+          this.field[this.y][this.x] = fieldCharacter;
+          this.x +=1;
+        }
         break;
 
       case 'l':
-        console.log('Move Left')
-        this.x -= 1;
+        if (this.field[this.y][this.x - 1] !== pathCharacter){
+          console.log('Move Left Forward');
+          this.x -= 1;
+        } else {
+          console.log('Move Left Backward')
+          this.field[this.y][this.x] = fieldCharacter;
+          this.x -=1;
+        }
         break;
       case 'u':
-        console.log('Move Up')
-        this.y -= 1;
+        if (this.field[this.y - 1][this.x] !== pathCharacter) {
+          console.log('Move Up Toward')
+          this.y -= 1;
+        } else {
+          console.log('Move Up Backward')
+          this.field[this.y][this.x] = fieldCharacter;
+          this.y -=1;
+        }
         break;
     
       case 'd':
-        console.log('Move Down')
-        this.y += 1;
+        if (this.field[this.y + 1][this.x] !== pathCharacter) {
+          console.log('Move Down Toward')
+          this.y += 1;
+        } else {
+          console.log('Move Down Backward')
+          this.field[this.y][this.x] = fieldCharacter;
+          this.y +=1;
+        }
         break;
     
       default:
+        console.log("Please enter valid character 'l','r','u','d' ");
         break;
     }
-
   }
   
   checkIfWin() {
-    if (this.field[this.y] == undefined) {
-      console.log('You lost, you fall outside of the field');
+    if (this.field[this.y] === undefined) {
+      console.log('You lost, you fall outside of the fieldy');
       stillPlaying = false;
     }
+    if (stillPlaying == true) {
+      switch (this.field[this.y][this.x]) {
+        case hole:
+          console.log('You lost, you fall into the hole');
+          stillPlaying = false;
+          break;
+  
+       case hat:
+          console.log('You found the hat, you win');
+          stillPlaying = false;
+          break;
+  
+       case fieldCharacter:
+          console.log('Keep going and look for the hat');
+          this.field[this.y][this.x] = pathCharacter;
+          stillPlaying = true;
+          break;
+  
+        case pathCharacter:
+          console.log('You move back, keep going to find the hat');
+          if (this.field[this.y][this.x + 1] == pathCharacter) {
+            this.field[this.y][this.x +1] = fieldCharacter;
+          }
 
-    switch (this.field[this.y][this.x]) {
-      case hole:
-        console.log('You lost, you fall into the hole');
-        stillPlaying = false;
-        break;
-     case hat:
-        console.log('You found the hat, you win');
-        stillPlaying = false;
-        break;
-     case fieldCharacter:
-        console.log('Keep going and look for the hat');
-        this.field[this.y][this.x] = pathCharacter;
-        stillPlaying = true;
-        break;
-      case undefined:
-        console.log('You lost, you fall outside of the field');
-        stillPlaying = false;
-        break;
+          // currentPositon = fieldCharacter;
+          // this.field[this.y][(this.x+1)] = fieldCharacter;
+          stillPlaying = true;
+          break;
 
-      default:
-        break;
+        case undefined:
+          console.log('You lost, you fall outside of the field');
+          stillPlaying = false;
+          break;
+        
+        default:
+          break;
+      }
     }
   }
+  
 
   static generateField(height, width, percentageOfHole) {
 
